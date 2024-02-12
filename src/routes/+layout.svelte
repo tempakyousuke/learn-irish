0b822721f15f59dc7c32b737dbcc35bfeee2a881
tempakyousuke <15189821+tempakyousuke@users.noreply.github.com>
2024-02-12 14:17:06 +0900

    prepare header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,24 @@
 </script>
 
 <div class="app">
-	<main>
+	<header class="flex border-b bg-teal-800 px-10">
+		<div class="text-white text-3xl grow my-auto">Learn Irish from hatao's youtube</div>
+		<a
+			href="/about"
+			class="text-white text-2xl ml-3 py-5 border-b-2 border-teal-800 hover:border-white">About</a
+		>
+		<a
+			href="/signin"
+			class="text-white text-2xl ml-3 py-5 border-b-2 border-teal-800 hover:border-white"
+			>ログイン</a
+		>
+		<a
+			href="/signup"
+			class="text-white text-2xl ml-3 py-5 border-b-2 border-teal-800 hover:border-white"
+			>新規登録</a
+		>
+	</header>
+	<main class="bg-teal-100 min-h-screen">
 		<slot />
 	</main>
 </div>
