fb4de9dc907b06f4592948a0c8a23d2538a9386a
tempakyousuke <15189821+tempakyousuke@users.noreply.github.com>
2024-04-06 18:11:19 +0900

    prepare rememberNameIds and rememberMelodyIds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
 	import type { Tune } from '../types/tune';
+	import type { UserTune } from '../types/userTune';
 	import TuneListItem from '$lib/tune/TuneListItem.svelte';
+	import { userStore } from '$modules/store';
+	import { getFirestore, collection, getDocs } from 'firebase/firestore';
 	export let data: {
 		tunes: Tune[];
 	};
 	const tunes = data.tunes;
+	const db = getFirestore();
+	let uid: string;
+	let rememberNameIds: string[] = [];
+	let rememberMelodyIds: string[] = [];
+
+	userStore.subscribe(async (value) => {
+		uid = value.uid;
+		if (!uid) {
+			return;
+		}
+		const tunesCollectionRef = collection(db, `users/${uid}/tunes`);
+		const querySnapshot = await getDocs(tunesCollectionRef);
+		const tunes = querySnapshot.docs.map((doc) => {
+			return { id: doc.id, ...doc.data() } as UserTune;
+		});
+		rememberNameIds = tunes.filter((tune) => tune.rememberName).map((tune) => tune.id);
+		rememberMelodyIds = tunes.filter((tune) => tune.rememberMelody).map((tune) => tune.id);
+	});
 </script>
 
 <svelte:head>
---
+++ src/types/userTune.ts
@@ -0,0 +1,6 @@
+export type UserTune = {
+	id: string;
+	rememberName: boolean;
+	rememberMelody: boolean;
+	playingLevel: number;
+};
