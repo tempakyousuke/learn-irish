4df725f040c8d9e31747caf935d42eb5c14cf85e
tempakyousuke <15189821+tempakyousuke@users.noreply.github.com>
2024-04-28 20:24:50 +0900

    enable to update data

--- src/routes/tune/update/[id]/+page.svelte
+++ src/routes/tune/update/[id]/+page.svelte
@@ -3,7 +3,10 @@
 	import { getYoutubeId } from '$modules/youtubeId';
 	import RadioButtons from '$lib/forms/RadioButtons.svelte';
 	import { userStore } from '$modules/store';
-	import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore';
+	import { getFirestore, doc, updateDoc } from 'firebase/firestore';
+	import Input from '$lib/forms/Input.svelte';
+	import Button from '$lib/button/Button.svelte';
+	import { update } from 'firebase/database';
 
 	// Firestoreのインスタンスを取得
 	const db = getFirestore();
@@ -12,29 +15,22 @@
 		tune: Tune;
 	};
 	const tune = data.tune;
+  const submit = () => {
+    const ref = doc(db, 'tunes', tune.id)
+    updateDoc(ref, tune)
+  }
+  console.log(tune)
 </script>
 
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
+
 <div>
-	<div class="row">
-		<div class="item-name">名前</div>
-		<div class="item-detail">{tune.name}</div>
-	</div>
+  <form>
+    <Input bind:value={tune.name} label="名前"/>
+    <Input bind:value={tune.link} label="link"/>
+    <Button className="mt-6" on:click={submit} >更新</Button>
+  </form>
 </div>
-
-<style type="postcss">
-	.row {
-		@apply flex mt-5;
-	}
-
-	.item-name {
-		@apply w-1/2 text-right pr-2;
-	}
-
-	.item-detail {
-		@apply w-1/2 pl-2;
-	}
-</style>
