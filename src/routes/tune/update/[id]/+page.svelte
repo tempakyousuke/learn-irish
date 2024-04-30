c11e236cb672f06de78268ea2fecbf0511c322a2
tempakyousuke <15189821+tempakyousuke@users.noreply.github.com>
2024-04-30 10:48:32 +0900

    remove unneccessary code

--- src/routes/tune/update/[id]/+page.svelte
+++ src/routes/tune/update/[id]/+page.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
 	import type { Tune } from '../../../../types/tune';
-	import { getYoutubeId } from '$modules/youtubeId';
-	import RadioButtons from '$lib/forms/RadioButtons.svelte';
-	import { userStore } from '$modules/store';
 	import { getFirestore, doc, updateDoc } from 'firebase/firestore';
 	import Input from '$lib/forms/Input.svelte';
 	import Button from '$lib/button/Button.svelte';
-	import { update } from 'firebase/database';
 
 	// Firestoreのインスタンスを取得
 	const db = getFirestore();
