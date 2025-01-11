62b5e982b5e09af4bbc36b6be90315b43d8dfb1f
tempakyousuke <15189821+tempakyousuke@users.noreply.github.com>
2025-01-11 19:45:08 +0900

    display key and mode

--- src/lib/tune/TuneList.svelte
+++ src/lib/tune/TuneList.svelte
@@ -8,7 +8,8 @@
 		<tr class="border bg-teal-800 text-white">
 			<th class="py-3 px-3 w-96">曲名</th>
 			<th class="py-3 px-3 w-52">種類</th>
-			<!-- <th class="py-3 px-3 w-52">回数</th> -->
+			<th class="py-3 px-3 w-52">Key</th>
+			<th class="py-3 px-3 w-52">Mode</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -22,6 +23,16 @@
 				<td class="py-3 px-3 text-center">
 					<a class="block" href="/tune/{tune.id}">
 						{tune.rhythm}
+					</a>
+				</td>
+				<td class="py-3 px-3 text-center">
+					<a class="block" href="/tune/{tune.id}">
+						{tune.key}
+					</a>
+				</td>
+				<td class="py-3 px-3 text-center">
+					<a class="block" href="/tune/{tune.id}">
+						{tune.mode}
 					</a>
 				</td>
 			</tr>
