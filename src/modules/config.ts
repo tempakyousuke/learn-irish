6684256c45d1d9f3d64519671954b6fb2444f9a1
tempakyousuke <15189821+tempakyousuke@users.noreply.github.com>
2025-05-14 01:51:12 +0900

    Remove unused authentication, configuration, favorites, firebase, date, tunes, i18n, statistics, store, user, and youtube ID modules

--- src/modules/config.ts
+++
@@ -1,2 +0,0 @@
-export const siteTitle = "Learn Irish from hatao's youtube";
-export const siteUrl = '';
--- src/modules/youtubeId.ts
+++
@@ -1,9 +0,0 @@
-export const getYoutubeId = (url?: string): string | null => {
-	if (!url) return null;
-	const regex =
-		/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
-
-	const match = url.match(regex);
-	const id = match ? match[1] : null;
-	return id;
-};
