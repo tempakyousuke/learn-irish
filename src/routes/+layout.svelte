27ee5e55628a9ed4ae3192b100744c6a4c2b65a7
tempakyousuke <15189821+tempakyousuke@users.noreply.github.com>
2025-07-29 15:53:23 +0900

    add youtube and pateon link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -50,7 +50,23 @@
 		<Header {openDrawer} />
 		<Drawer isOpen={drawer} {closeDrawer} {handleKeydown} />
 
-		<div class="flex bg-teal-50 justify-end pr-4 pt-2">
+		<div class="flex bg-teal-50 justify-end items-center gap-4 pr-4 pt-2">
+			<a 
+				href="https://www.youtube.com/@hatao" 
+				target="_blank" 
+				rel="noopener noreferrer"
+				class="text-blue-600 hover:text-blue-800 underline"
+			>
+				Youtube
+			</a>
+			<a 
+				href="https://www.patreon.com/tune_everyday" 
+				target="_blank" 
+				rel="noopener noreferrer"
+				class="text-blue-600 hover:text-blue-800 underline"
+			>
+				Patreon
+			</a>
 			<LanguageSwitcher />
 		</div>
 		<main class="bg-teal-50 min-h-screen pt-10">
